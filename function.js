ae691cb0f9adda53797abb06fc852a5e6846d478
eustacheAki <eustacheeussy@gmail.com>
2020-08-15 18:53:02 +0200

    adding function

---
+++ function.js
@@ -0,0 +1,11 @@
+const alphabetCount = ()=>{
+    const output =`the length of shema is: ${'shema'.length}`;
+
+return output
+}
+
+// function alphabetCount(word){
+
+// }
+
+console.log(alphabetCount(), typeof alphabetCount());
